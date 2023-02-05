b459b0a912e332f96570c7f87be6794d442d8f59
lubiksss <lubiksss@gmail.com>
2023-02-05 14:41:14 +0900

    feat:

--- main.dart
+++ main.dart
@@ -1,31 +1,18 @@
-class Human {
-  final String name;
-
-  Human(this.name);
-
-  void sayHello() {
-    print('Hello, I am $name');
-  }
-}
-
 enum Team { blue, red }
 
-class Player extends Human {
+class Player with Strong{
   final Team team;
 
   Player({
     required this.team,
     required String name,
-  }) : super(name);
+  });
+}
 
-  @override
-  void sayHello() {
-    super.sayHello();
-    print('and I play for team $team');
-  }
+class Strong{
+  final double strength = 1500.99;
 }
 
 void main() {
   final player = Player(team: Team.blue, name: 'John');
-  player.sayHello();
 }
